6dc5e690e8da25c3ea1730d58a6a6a0c4522f250
pdbautista0 <patdi.bautista@gmail.com>
2023-03-20 15:48:53 +0800

    Added Book object

---
+++ src/Book.js
@@ -0,0 +1,9 @@
+function Book(title, author, page, isDoneReading, coverColor) {
+    this.title = title;
+    this.author = author;
+    this.page = page;
+    this.isDoneReading = isDoneReading;
+    this.coverColor = coverColor;
+  }
+
+export default Book;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Book from "./Book.js";
+
+let book1 = new Book("funds", "patrick", 200, false, "#fefefe");
+
+console.log(book1);

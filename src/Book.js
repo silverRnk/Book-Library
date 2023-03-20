5608d12887d082ba8e98beae1b7066c2231cee2c
pdbautista0 <patdi.bautista@gmail.com>
2023-03-21 01:48:02 +0800

    Edit book edit bookdisplay

--- src/Book.js
+++ src/Book.js
@@ -1,9 +1,15 @@
-function Book(title, author, page, isDoneReading, coverColor) {
-    this.title = title;
-    this.author = author;
-    this.page = page;
-    this.isDoneReading = isDoneReading;
-    this.coverColor = coverColor;
-  }
+function Book(
+  bookTitle = '',
+  author = '',
+  page = 0,
+  isDoneReading = false,
+  coverColor = "#ffffff"
+) {
+  this.bookTitle = bookTitle;
+  this.author = author;
+  this.page = page;
+  this.isDoneReading = isDoneReading;
+  this.coverColor = coverColor;
+}
 
 export default Book;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,110 @@
 import Book from "./Book.js";
 
-let book1 = new Book("funds", "patrick", 200, false, "#fefefe");
+let bookList = [];
 
-console.log(book1);
+function addBookToList(book) {
+  if(bookList.length != 0){
+    bookList = [book].concat(bookList)
+  }else{
+    bookList = [book]
+  }
+}
+
+function removeBookToList(index) {
+  bookList = bookList.filter((value, i) => {
+    i != index;
+  });
+}
+
+function editBookFromList(book, index) {
+  bookList[index] = book;
+}
+
+function updateCabinet() {
+  let bookCabinet = document.getElementById("cabinet");
+  /*
+      case 0 < list.length < 3
+
+      case 2 < list.length < 6
+
+      case 6 < list.length
+      */
+  bookCabinet.innerHTML =
+    '<div class="shelves" level="1">' +
+    '<div class="book-column">' +
+    '<div class="bookItem"></div>' +
+    '</div></div><div class="shelves" level="2">' +
+    "</div></div>";
+
+  if (bookList.length != 0) {
+    bookList.forEach((element, index) => {
+      if (-1 < index && index <= 1) {
+        let shelves = bookCabinet.querySelector(
+          '.shelves[level="1"]'
+        );
+        shelves.innerHTML =
+          shelves.innerHTML +
+          '<div class="book-column">' +
+          '<div class="bookItem"></div>' +
+          "</div>";
+        console.log(shelves);
+      } else if (2 <= index && index <= 4) {
+        let shelves = bookCabinet.querySelector(
+          '.shelves[level="2"]'
+        );
+        shelves.innerHTML =
+          shelves.innerHTML +
+          '<div class="book-column">' +
+          '<div class="bookItem"></div>' +
+          "</div>";
+      } else if (5 <= index) {
+        /*
+        case index + 1 % 3 == 0
+        add new shelves then add new book
+        else
+        add new books
+        */
+        let shelves;
+        const shelvesLevel = Math.floor((index + 1) / 3) + 1;
+        if ((index + 1) % 3 == 0) {
+          bookCabinet.innerHTML =
+            bookCabinet.innerHTML +
+            '<div class="shelves"' +
+            `level=${shelvesLevel}></div>`;
+          bookCabinet = document.getElementById("cabinet");
+          shelves = bookCabinet.querySelector(
+            `.shelves[level="${shelvesLevel}"]`
+          );
+          shelves.innerHTML =
+            shelves.innerHTML +
+            '<div class="book-column">' +
+            '<div class="bookItem"></div>' +
+            "</div>";
+        } else {
+          shelves = bookCabinet.querySelector(
+            `.shelves[level="${shelvesLevel}"]`
+          );
+          shelves.innerHTML =
+            shelves.innerHTML +
+            '<div class="book-column">' +
+            '<div class="bookItem"></div>' +
+            "</div>";
+        }
+      }
+
+      bookCabinet = document.getElementById("cabinet");
+    });
+  }
+}
+
+function onClickAddBook() {
+  let addBookBtn = document.querySelector("#add-book");
+  addBookBtn.addEventListener("click", () => {
+    addBookToList(new Book("fsfs", "fsfs", 100));
+    updateCabinet();
+    onClickAddBook();
+    console.log(bookList.length);
+  });
+}
+
+onClickAddBook();
